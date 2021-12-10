df859b5232693eec81fdb5253cd5399996094828
Fondem Junior <fondemjunior@yahoo.com>
2021-12-10 16:09:01 +0100

    connect to API

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
 import BookList from '../components/BookList';
 import InputBook from '../components/InputBook';
+import { loadAsync } from '../redux/books/books';
 
 const Books = () => {
+  const dispatch = useDispatch();
+  const [loadBook, setLoadBook] = useState('idle');
+
+  useEffect(() => {
+    if (loadBook === 'idle')
+    return () => {
+      dispatch(loadAsync());
+      setLoadBook('done')
+    }
+  })
+
   const books = useSelector((state) => state.booksReducer);
   return (
     <div>
